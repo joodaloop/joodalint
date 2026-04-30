ffc89f358717dc4aec31f4d0a1dceb933533cf89
joodaloop <d4js2468@gmail.com>
2026-04-30 17:18:44 +0530

    Defining new interface for the new parsing pipeline

--- internal/rules/rules.go
+++ internal/rules/rules.go
@@ -43,7 +43,33 @@ type MarkdownContext struct {
 	Config *config.Config
 }
 
+type FrontmatterFile struct {
+	Path   string
+	Raw    []byte
+	Parsed map[string]any
+	Line0  int
+}
+
+type FrontmatterContext struct {
+	Config *config.Config
+}
+
 type MarkdownRule interface {
+	ID() string
+	Check(f *MarkdownFile, ctx *MarkdownContext) []Diagnostic
+}
+
+type FrontmatterRule interface {
+	ID() string
+	Check(f *FrontmatterFile, ctx *FrontmatterContext) []Diagnostic
+}
+
+type MarkdownASTRule interface {
+	ID() string
+	Check(f *MarkdownFile, ctx *MarkdownContext) []Diagnostic
+}
+
+type MarkdownTextRule interface {
 	ID() string
 	Check(f *MarkdownFile, ctx *MarkdownContext) []Diagnostic
 }
@@ -54,12 +80,21 @@ type HTMLRule interface {
 }
 
 var (
-	markdownRules []MarkdownRule
-	htmlRules     []HTMLRule
+	markdownRules    []MarkdownRule
+	frontmatterRules []FrontmatterRule
+	astRules         []MarkdownASTRule
+	textRules        []MarkdownTextRule
+	htmlRules        []HTMLRule
 )
 
-func RegisterMarkdown(r MarkdownRule) { markdownRules = append(markdownRules, r) }
-func RegisterHTML(r HTMLRule)         { htmlRules = append(htmlRules, r) }
+func RegisterMarkdown(r MarkdownRule)         { markdownRules = append(markdownRules, r) }
+func RegisterFrontmatter(r FrontmatterRule)   { frontmatterRules = append(frontmatterRules, r) }
+func RegisterMarkdownAST(r MarkdownASTRule)   { astRules = append(astRules, r) }
+func RegisterMarkdownText(r MarkdownTextRule) { textRules = append(textRules, r) }
+func RegisterHTML(r HTMLRule)                 { htmlRules = append(htmlRules, r) }
 
-func Markdown() []MarkdownRule { return markdownRules }
-func HTML() []HTMLRule         { return htmlRules }
+func Markdown() []MarkdownRule         { return markdownRules }
+func Frontmatter() []FrontmatterRule   { return frontmatterRules }
+func MarkdownAST() []MarkdownASTRule   { return astRules }
+func MarkdownText() []MarkdownTextRule { return textRules }
+func HTML() []HTMLRule                 { return htmlRules }
